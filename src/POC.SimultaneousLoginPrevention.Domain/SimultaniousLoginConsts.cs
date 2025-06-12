b3c4f091b8cf4699b2c5bab7886a96a9ec8effb9
Mostafa Labib <mostafal@ejada.com>
2025-06-12 10:27:27 +0300

    Prevent concurrent login using Concurrent Login column against user claim.

---
+++ src/POC.SimultaneousLoginPrevention.Domain/SimultaniousLoginConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace OCR.SimultaniousLogin;
+
+public static class SimultaniousLoginConsts
+{
+    public const string DbTablePrefix = "App";
+
+    public const string DbSchema = null;
+    public const string ConcurrentLoginToken = "ConcurrentLogin";
+}
---
+++ src/POC.SimultaneousLoginPrevention.HttpApi.Host/SimultaniousLoginHttpApiHostModule.cs
@@ -0,0 +1,333 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OCR.SimultaniousLogin.EntityFrameworkCore;
+using OCR.SimultaniousLogin.MultiTenancy;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.LeptonXLite;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.LeptonXLite.Bundling;
+using Microsoft.OpenApi.Models;
+using OpenIddict.Validation.AspNetCore;
+using Volo.Abp;
+using Volo.Abp.Account;
+using Volo.Abp.Account.Web;
+using Volo.Abp.AspNetCore.MultiTenancy;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
+using Volo.Abp.AspNetCore.Serilog;
+using Volo.Abp.Autofac;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.Security.Claims;
+using Volo.Abp.Swashbuckle;
+using Volo.Abp.UI.Navigation.Urls;
+using Volo.Abp.VirtualFileSystem;
+using Microsoft.AspNetCore.Identity;
+using ConcurrentLogin.AspNetIdentity;
+using Volo.Abp.AspNetCore.Mvc.Localization;
+using OCR.SimultaniousLogin.Localization;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Volo.Abp.IdentityServer;
+using Microsoft.AspNetCore.Authentication;
+using Volo.Abp.Identity.AspNetCore;
+using Volo.Abp.Identity;
+using Volo.Abp.Users;
+using Volo.Abp.IdentityServer.Jwt;
+using ConcurrentLogin;
+using Volo.Abp.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace OCR.SimultaniousLogin;
+
+[DependsOn(
+    typeof(SimultaniousLoginHttpApiModule),
+    typeof(AbpAutofacModule),
+    typeof(AbpAspNetCoreMultiTenancyModule),
+    typeof(SimultaniousLoginApplicationModule),
+    typeof(SimultaniousLoginEntityFrameworkCoreModule),
+    typeof(AbpAspNetCoreMvcUiLeptonXLiteThemeModule),
+    typeof(AbpAccountWebOpenIddictModule),
+    typeof(AbpAspNetCoreSerilogModule),
+    typeof(AbpSwashbuckleModule),
+    typeof(SimultaniousLoginApplicationModule)
+)]
+public class SimultaniousLoginHttpApiHostModule : AbpModule
+{
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        PreConfigure<IdentityBuilder>(builder =>
+        {
+            builder.AddSignInManager<ConcurrentLoginSignInManager>();
+        });
+
+
+        context.Services.PreConfigure<AbpMvcDataAnnotationsLocalizationOptions>(options =>
+        {
+            options.AddAssemblyResource(
+               typeof(SimultaniousLoginResource),
+               typeof(SimultaniousLoginDomainModule).Assembly,
+               typeof(SimultaniousLoginDomainSharedModule).Assembly,
+               typeof(SimultaniousLoginApplicationModule).Assembly,
+               typeof(SimultaniousLoginApplicationContractsModule).Assembly
+           );
+        });
+
+        PreConfigure<OpenIddictBuilder>(builder =>
+        {
+            builder.AddValidation(options =>
+            {
+                options.AddAudiences("SimultaniousLogin");
+                options.UseLocalServer();
+                options.UseAspNetCore();
+            });
+        });
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+        Configure<SecurityStampValidatorOptions>(options =>
+        {
+            options.ValidationInterval = TimeSpan.Zero; ;
+            options.OnRefreshingPrincipal = principalContext =>
+            {
+                var oldToken = principalContext.CurrentPrincipal.Identities.FirstOrDefault()?.Claims.FirstOrDefault(c => c.Type == SimultaniousLoginConsts.ConcurrentLoginToken);
+                if (oldToken != null)
+                {
+                    principalContext.NewPrincipal.Identities.FirstOrDefault()?.AddOrReplace(new Claim(SimultaniousLoginConsts.ConcurrentLoginToken, oldToken.Value));
+                }
+                return Task.CompletedTask;
+            };
+        });
+
+        Configure<AbpClaimsServiceOptions>(options =>
+        {
+            options.RequestedClaims.AddRange(new[]{
+                SimultaniousLoginConsts.ConcurrentLoginToken
+            });
+        });
+
+
+        ConfigureAuthentication(context);
+        ConfigureBundles();
+        ConfigureUrls(configuration);
+        ConfigureConventionalControllers();
+        ConfigureVirtualFileSystem(context);
+        ConfigureCors(context, configuration);
+        ConfigureSwaggerServices(context, configuration);
+    }
+
+    private void ConfigureAuthentication(ServiceConfigurationContext context)
+    {
+        context.Services.ForwardIdentityAuthenticationForBearer(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+        context.Services.Configure<AbpClaimsPrincipalFactoryOptions>(options =>
+        {
+            options.IsDynamicClaimsEnabled = true;
+        });
+    }
+
+    private void ConfigureBundles()
+    {
+        Configure<AbpBundlingOptions>(options =>
+        {
+            options.StyleBundles.Configure(
+                LeptonXLiteThemeBundles.Styles.Global,
+                bundle =>
+                {
+                    bundle.AddFiles("/global-styles.css");
+                }
+            );
+        });
+    }
+
+    private void ConfigureUrls(IConfiguration configuration)
+    {
+        Configure<AppUrlOptions>(options =>
+        {
+            options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
+            options.RedirectAllowedUrls.AddRange(configuration["App:RedirectAllowedUrls"]?.Split(',') ?? Array.Empty<string>());
+
+            options.Applications["Angular"].RootUrl = configuration["App:ClientUrl"];
+            options.Applications["Angular"].Urls[AccountUrlNames.PasswordReset] = "account/reset-password";
+        });
+    }
+
+    private void ConfigureVirtualFileSystem(ServiceConfigurationContext context)
+    {
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+        if (hostingEnvironment.IsDevelopment())
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.ReplaceEmbeddedByPhysical<SimultaniousLoginDomainSharedModule>(
+                    Path.Combine(hostingEnvironment.ContentRootPath,
+                        $"..{Path.DirectorySeparatorChar}POC.SimultaneousLoginPrevention.Domain.Shared"));
+                options.FileSets.ReplaceEmbeddedByPhysical<SimultaniousLoginDomainModule>(
+                    Path.Combine(hostingEnvironment.ContentRootPath,
+                        $"..{Path.DirectorySeparatorChar}POC.SimultaneousLoginPrevention.Domain"));
+                options.FileSets.ReplaceEmbeddedByPhysical<SimultaniousLoginApplicationContractsModule>(
+                    Path.Combine(hostingEnvironment.ContentRootPath,
+                        $"..{Path.DirectorySeparatorChar}POC.SimultaneousLoginPrevention.Application.Contracts"));
+                options.FileSets.ReplaceEmbeddedByPhysical<SimultaniousLoginApplicationModule>(
+                    Path.Combine(hostingEnvironment.ContentRootPath,
+                        $"..{Path.DirectorySeparatorChar}POC.SimultaneousLoginPrevention.Application"));
+            });
+        }
+    }
+
+    private void ConfigureConventionalControllers()
+    {
+        Configure<AbpAspNetCoreMvcOptions>(options =>
+        {
+            options.ConventionalControllers.Create(typeof(SimultaniousLoginApplicationModule).Assembly);
+        });
+    }
+
+    private static void ConfigureSwaggerServices(ServiceConfigurationContext context, IConfiguration configuration)
+    {
+        context.Services.AddAbpSwaggerGenWithOAuth(
+            configuration["AuthServer:Authority"]!,
+            new Dictionary<string, string>
+            {
+                    {"SimultaniousLogin", "SimultaniousLogin API"}
+            },
+            options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "SimultaniousLogin API", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
+                options.CustomSchemaIds(type => type.FullName);
+            });
+    }
+
+    private void ConfigureCors(ServiceConfigurationContext context, IConfiguration configuration)
+    {
+        context.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder
+                    .WithOrigins(configuration["App:CorsOrigins"]?
+                        .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(o => o.RemovePostFix("/"))
+                        .ToArray() ?? Array.Empty<string>())
+                    .WithAbpExposedHeaders()
+                    .SetIsOriginAllowedToAllowWildcardSubdomains()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
+        });
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseAbpRequestLocalization();
+
+        if (!env.IsDevelopment())
+        {
+            app.UseErrorPage();
+        }
+
+        app.UseCorrelationId();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseCors();
+        app.UseAuthentication();
+        app.Use(async (httpContext, next) =>
+        {
+
+            var currentUser = httpContext.RequestServices.GetRequiredService<ICurrentUser>();
+
+            if (currentUser.IsAuthenticated)
+
+            {
+
+                var claimToken = currentUser.GetConcurrentLoginToken();
+
+                var userManager = httpContext.RequestServices.GetRequiredService<IdentityUserManager>();
+
+                var user = await userManager.FindByIdAsync(currentUser.Id.ToString());
+
+                if (claimToken != user.GetProperty(SimultaniousLoginConsts.ConcurrentLoginToken).ToString())
+
+                {
+
+                    //Cookies
+
+                    if (httpContext.User.Identity != null && httpContext.User.Identity.AuthenticationType == "Identity.Application")
+
+                    {
+
+                        await httpContext.RequestServices.GetRequiredService<AbpSignInManager>().SignOutAsync();
+
+                        await httpContext.ChallengeAsync("Identity.Application");
+
+                    }
+
+                    //JWT
+
+                    if (httpContext.User.Identity != null && httpContext.User.Identity.AuthenticationType == "AuthenticationTypes.Federation")
+
+                    {
+
+                        await httpContext.ChallengeAsync(JwtBearerDefaults.AuthenticationScheme);
+
+                    }
+
+                    //Other
+
+                    return;
+
+                }
+
+            }
+
+            await next();
+
+        });
+
+        app.UseAbpOpenIddictValidation();
+
+        if (MultiTenancyConsts.IsEnabled)
+        {
+            app.UseMultiTenancy();
+        }
+        app.UseUnitOfWork();
+        app.UseDynamicClaims();
+        app.UseAuthorization();
+
+        app.UseSwagger();
+        app.UseAbpSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimultaniousLogin API");
+
+            var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
+            c.OAuthClientId(configuration["AuthServer:SwaggerClientId"]);
+            c.OAuthScopes("SimultaniousLogin");
+        });
+
+        app.UseAuditing();
+        app.UseAbpSerilogEnrichers();
+        app.UseConfiguredEndpoints();
+    }
+}
